694860cb2ab1448965da5d26680a0d39ac163764
Abhijit Suresh <abhijit5903@gmail.com>
2024-11-21 17:28:32 -0700

    Add Doc strings

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ from db_util import *
 app = Flask(__name__)
 
 with app.app_context():
+    """
+    Runs one time before the first request to the application.
+    """
     conn = sqlite3.connect('url.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS urls \
@@ -18,10 +21,28 @@ chars = string.ascii_letters + string.digits
 
 @app.route('/success/<url>')
 def success(url):
+    """
+    Generates an HTML string for a shortened URL.
+
+    Args:
+        url (str): The shortened URL path.
+
+    Returns:
+        str: An HTML string containing the shortened URL link.
+    """
     return 'Shortened URL: <a href="http://127.0.0.1:5000/url/%s"> http://127.0.0.1:5000/url/%s </a>' %( url, url)
 
 @app.route('/url/<url>')
 def access_url(url):
+    """
+    Accesses a given URL, checks if it has expired, and redirects to the long URL if it hasn't expired.
+
+    Args:
+        url (str): The shortened URL to be accessed.
+
+    Returns:
+        str: A message indicating that the URL has expired, or a redirect to the long URL.
+    """
     if check_expiry(url):
         delete_url(url)
         return 'URL has expired'
@@ -29,12 +50,34 @@ def access_url(url):
     return redirect(long_url)
 
 def hash(input):
+    """
+    Generates a random hash string.
+
+    Args:
+        input: The input value to be hashed (not used in the current implementation).
+
+    Returns:
+        str: A randomly generated hash string.
+    """
     short = ''.join(secrets.choice(chars) for i in range(length))
     return short
 
 
 @app.route('/create_url', methods=['GET', 'POST'])
 def create_url():
+    """
+    Handles the creation of a shortened URL.
+
+    If the request method is POST, it retrieves the long URL and expiry date from the form data,
+    generates a hash for the long URL, and checks if the hash is available. If available, it inserts
+    the URL data into the database and redirects to the success page with the shortened URL hash.
+    If the hash is not available, it redirects back to the URL creation page.
+
+    Returns:
+        Response: A redirect to the success page with the shortened URL hash if the URL is successfully
+                  shortened, or a redirect back to the URL creation page if the hash is not available.
+                  If the request method is not POST, it renders the index page.
+    """
     if request.method == 'POST':
         long_url = request.form['url']
         expiry = request.form['expiry']
@@ -51,5 +94,11 @@ if __name__ == '__main__':
 
 @app.route("/")
 def index():
+    """
+    Renders the index.html template.
+
+    Returns:
+        str: The rendered HTML content of the index page.
+    """
     return render_template("index.html")
 
--- src/db_util.py
+++ src/db_util.py
@@ -3,6 +3,15 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 def check_expiry(hash):
+    """
+    Check if the URL associated with the given hash has expired.
+
+    Args:
+        hash (str): The hash of the URL to check.
+
+    Returns:
+        bool: True if the URL has expired, False otherwise.
+    """
     conn = sqlite3.connect('url.db')
     c = conn.cursor()
     result = c.execute("SELECT expiry < datetime('now') FROM urls WHERE hash = ?", (hash,))
@@ -13,6 +22,15 @@ def check_expiry(hash):
         return False
 
 def fetch_url(hash):
+    """
+    Fetches the long URL corresponding to the given hash from the database.
+
+    Args:
+        hash (str): The hash value used to look up the long URL in the database.
+
+    Returns:
+        str: The long URL if found, otherwise an empty string.
+    """
     conn = sqlite3.connect('url.db')
     c = conn.cursor()
     result = c.execute("SELECT long_url FROM urls WHERE hash = ?", (hash,))
@@ -22,12 +40,33 @@ def fetch_url(hash):
     return output[0]
 
 def check_availability(hash):
+    """
+    Check if a given hash is available in the 'urls' table of the 'url.db' database.
+
+    Args:
+        hash (str): The hash value to check for availability.
+
+    Returns:
+        bool: True if the hash is not present in the 'urls' table, False otherwise.
+    """
     conn = sqlite3.connect('url.db')
     c = conn.cursor()
     result = c.execute("SELECT EXISTS (SELECT 1 from urls WHERE hash = ?)", (hash,))
     return True if result.fetchone()[0] == 0 else False
 
 def insert_url(hash, long_url, short_url, expiry):
+    """
+    Inserts a URL record into the database.
+
+    Args:
+        hash (str): The unique hash for the URL.
+        long_url (str): The original long URL.
+        short_url (str): The shortened URL.
+        expiry (str): The expiry date in the format 'YYYY-MM-DD'. If None, the expiry date will be set to one year from the current date.
+
+    Returns:
+        None
+    """
     now = datetime.now() 
     if expiry:	
         expiry_date = datetime.strptime(expiry, "%Y-%m-%d")
@@ -41,12 +80,32 @@ def insert_url(hash, long_url, short_url, expiry):
     conn.commit()
 
 def delete_url(hash):
+    """
+    Deletes a URL entry from the 'urls' table in the 'url.db' SQLite database based on the provided hash.
+
+    Args:
+        hash (str): The hash value of the URL entry to be deleted.
+
+    Returns:
+        None
+    """
     conn = sqlite3.connect('url.db')
     c = conn.cursor()
     c.execute("DELETE FROM urls WHERE hash = ?", (hash,))
     conn.commit()
 
 def delete_expired_urls():
+    """
+    Deletes expired URLs from the 'urls' table in the 'url.db' SQLite database.
+
+    This function connects to the 'url.db' SQLite database, executes a SQL 
+    DELETE statement to remove rows from the 'urls' table where the 'expiry' 
+    column is less than the current datetime, and commits the changes.
+
+    Raises:
+        sqlite3.DatabaseError: If there is an issue with the database connection 
+        or execution of the SQL statement.
+    """
     conn = sqlite3.connect('url.db')
     c = conn.cursor()
     c.execute("DELETE FROM urls WHERE expiry < datetime('now')")
